97a892ee5a7494585c71283772aa99a8897152c5
ADDANKI DINESH <80439638+addankidinesh@users.noreply.github.com>
2022-04-24 10:43:37 +0530

    Update App.java

--- App.java
+++ App.java
@@ -3,16 +3,16 @@ import java.util.List;
 
 public class App {
 	public static void main(String[] args) {
-		List<String> tempTxList = new ArrayList<String>();
-		tempTxList.add("Mumbai Indians");
-		tempTxList.add("are");
-		tempTxList.add("5");
-		tempTxList.add("Times");
-		tempTxList.add("Champions");
-        tempTxList.add("of IPL");
-
-		MerkleTree merkleTrees = new MerkleTree(tempTxList);
+		List<String> ChildList = new ArrayList<String>();
+		ChildList.add("Mumbai Indians");
+		ChildList.add("are");
+		ChildList.add("5");
+		ChildList.add("Times");
+		ChildList.add("Champions");
+        ChildList.add("of IPL");
+		MerkleTree merkleTrees = new MerkleTree(ChildList);
 		merkleTrees.merkle_tree();
-		System.out.println("root : " + merkleTrees.getRoot());
+		System.out.println("Hash of root or whole tree :");
+		System.out.println(merkleTrees.getRoot());
 	}
 }
